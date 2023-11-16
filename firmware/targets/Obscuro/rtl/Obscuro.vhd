cbb8a2976a968836cc184ce8d44cd50a715090b9
Caleb Fletcher <caleb@fletcher.cf>
2023-11-16 23:31:42 +1100

    feat: update firmware to have a clock and reset

--- firmware/targets/Obscuro/rtl/Obscuro.vhd
+++ firmware/targets/Obscuro/rtl/Obscuro.vhd
@@ -1,6 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 
 library xpm;
@@ -8,14 +9,29 @@ use xpm.vcomponents.all;
 
 entity Obscuro is
     port (
-        a : in STD_LOGIC;
-        b : in STD_LOGIC;
-        q : out STD_LOGIC
+        CLK100MHZ : in STD_LOGIC;
+        ck_rst : in STD_LOGIC;
+        btn : in STD_LOGIC_VECTOR (0 downto 0);
+        led : out STD_LOGIC_VECTOR (0 downto 0)
     );
 end Obscuro;
 
 
 architecture Behavioural of Obscuro is
+    signal counter: STD_LOGIC_VECTOR (3 downto 0);
 begin
-    q <= a and b;
+    led(0) <= CLK100MHZ;
+
+    process (CLK100MHZ)
+    begin
+        if rising_edge(CLK100MHZ) then
+            if ck_rst = '0' then
+                -- Reset
+                counter <= "0";
+            else
+                -- Normal operation
+                --counter <= counter + std_logic_vector(1);
+            end if;
+        end if;
+    end process;
 end Behavioural;
