a402239a5427bdbcc5c08d73ad8b94f6d0962fcc
Caleb Fletcher <caleb@fletcher.cf>
2023-11-21 00:37:53 +1100

    feat: add basic fsm for reset/presence logic

--- firmware/targets/Obscuro/rtl/OneWireSlave.vhd
+++ firmware/targets/Obscuro/rtl/OneWireSlave.vhd
@@ -4,6 +4,9 @@ library IEEE;
     use IEEE.NUMERIC_STD.all;
 
 entity OneWireSlave is
+    generic (
+        F_CLK_KHZ : natural := 100
+    );
     port (
         -- 100KHz/10us clock
         clk  : in    STD_LOGIC;
@@ -12,20 +15,76 @@ entity OneWireSlave is
 end entity;
 
 architecture Behavioural of OneWireSlave is
-begin
-    data <= 'Z';
+    type state_type is (WAIT_FOR_RESET, WAIT_FOR_PRESENCE, PRESENCE, WAIT_FOR_COMMAND);
+    signal pr_state : state_type := WAIT_FOR_RESET;
+    signal nx_state : state_type;
 
+    signal timer     : natural := 0;
+    signal timer_max : natural := 0;
+
+    -- family is 0x28
+    --constant ID : std_logic_vector := x"E800000B1FCD1028";
+begin
+    -- Register for state
     process (clk)
     begin
         if rising_edge(clk) then
-            if data = '0' or data = 'L' then
-                report "detected low";
+            pr_state <= nx_state;
+        end if;
+    end process;
 
+    -- Logic for state transitions
+    process (all)
+    begin
+        case pr_state is
+            when WAIT_FOR_RESET =>
+                if not data then
+                    -- Detected start of a reset pulse
+                    nx_state <= WAIT_FOR_PRESENCE;
+                end if;
+            when WAIT_FOR_PRESENCE =>
+                if timer = timer_max then
+                    nx_state <= PRESENCE;
+                end if;
+            when PRESENCE =>
+                nx_state <= WAIT_FOR_COMMAND;
+                -- TODO: set a timeout?
+            when WAIT_FOR_COMMAND =>
+        end case;
+    end process;
+
+    -- Logic for output
+    process (all)
+    begin
+        if pr_state = PRESENCE then
+            data <= '0';
+        else
+            data <= 'Z';
+        end if;
+    end process;
+
+    -- Timer
+    process (all)
+    begin
+        case pr_state is
+            when WAIT_FOR_PRESENCE =>
                 -- wait for tPDH (15us-60us)
-                -- pull low for presence signal
+                timer_max <= 3;
+            when PRESENCE =>
+                timer_max <= 10;
                 -- wait for tPDL (60us-240us)
-                -- 
+            when others =>
+                timer_max <= 0;
+        end case;
+
+        if rising_edge(clk) then
+            if pr_state /= nx_state then
+                -- state transition, reset timer
+                timer <= 0;
+            elsif timer /= timer_max then
+                timer <= timer + 1;
             end if;
         end if;
     end process;
+
 end architecture;
--- firmware/targets/Obscuro/tb/OneWireSlaveTb.vhd
+++ firmware/targets/Obscuro/tb/OneWireSlaveTb.vhd
@@ -10,8 +10,46 @@ end entity;
 
 architecture test of OneWireSlaveTb is
     -- 100KHz/10us clock
-    signal clk  : STD_LOGIC := '1';
-    signal data : STD_LOGIC;
+    signal clk                 : STD_LOGIC := '1';
+    signal data                : STD_LOGIC;
+    signal master_release_time : delay_length;
+    signal rx_value            : std_logic;
+
+    procedure write_bit(signal data : out std_logic; constant value : in boolean) is
+        variable slot_start : delay_length;
+    begin
+        slot_start := now;
+        -- pull low
+        data <= '0';
+        if value then
+            -- wait t1l
+            wait for 10 us;
+        else
+            -- wait t0l
+            wait for 60 us;
+        end if;
+        -- release
+        data <= 'Z';
+        -- wait for remaining tslot
+        wait for (80 us + slot_start) - now;
+    end procedure;
+
+    procedure read_bit(signal data : inout std_logic; signal value : out std_logic) is
+        variable slot_start : delay_length;
+    begin
+        slot_start := now;
+        -- pull low
+        data <= '0';
+        -- wait tRL
+        wait for 5 us;
+        -- release
+        data <= 'Z';
+        -- wait for tMSR after slot start
+        wait for 10 us;
+        value <= data;
+        -- wait for remaining tslot
+        wait for (80 us + slot_start) - now;
+    end procedure;
 begin
     -- Instantiate DUT
     dut: entity work.OneWireSlave
@@ -26,14 +64,13 @@ begin
     -- Generate the test stimulus
 
     test: process
-        variable master_release_time : delay_length;
     begin
         wait for 20 us;
         -- reset pulse  tRSTL (480us to 640us)
         data <= '0';
         wait for 480 us;
         data <= 'Z';
-        master_release_time := now;
+        master_release_time <= now;
 
         -- wait for tMSP (min 68us max 75us)
         wait for 70 us;
@@ -44,10 +81,11 @@ begin
         end if;
 
         -- wait for tRSTH since we went high-impedance
-        wait for 480 us - master_release_time;
+        wait for (master_release_time + 480 us) - now;
 
         -- Can start data communications now
-        --data <= '0';
+        read_bit(data, rx_value);
+        report "got value " & to_string(rx_value);
         -- Testing complete
         report "##### TESTBENCH COMPLETE #####";
         finish;
